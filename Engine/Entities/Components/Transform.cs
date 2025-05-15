471a5486bdfb7c881fa6e3031822f6b18d8663e7
JakeOliver-UK <jake_oliver@live.co.uk>
2025-05-15 15:52:13 +0100

    Change math funtions in Transform component to use matrices instead (for performance gains).

--- Engine/Entities/Components/Transform.cs
+++ Engine/Entities/Components/Transform.cs
@@ -5,71 +5,104 @@ namespace Colonia.Engine.Entities.Components
 {
     internal class Transform : Component
     {
-        public Vector2 LocalPosition { get; set; } = Vector2.Zero;
-        public float LocalRotation { get; set; } = 0f;
-        public Vector2 LocalScale { get; set; } = new Vector2(1f, 1f);
+        private Vector2 _localPosition = Vector2.Zero;
+        private float _localRotation = 0f;
+        private Vector2 _localScale = Vector2.One;
+        private bool _localMatrixDirty = true;
+
+        private Matrix _localMatrix = Matrix.Identity;
+        private Matrix _worldMatrix = Matrix.Identity;
+
+        private Vector2 _position;
+        private float _rotation;
+        private Vector2 _scale;
+
+        public Vector2 LocalPosition
+        {
+            get => _localPosition;
+            set { _localPosition = value; _localMatrixDirty = true; }
+        }
+
+        public float LocalRotation
+        {
+            get => _localRotation;
+            set { _localRotation = value; _localMatrixDirty = true; }
+        }
+
+        public Vector2 LocalScale
+        {
+            get => _localScale;
+            set { _localScale = value; _localMatrixDirty = true; }
+        }
+
+        public Matrix WorldMatrix => _worldMatrix;
 
         public Vector2 Position
         {
-            get
-            {
-                if (Entity.IsRoot) return LocalPosition;
-
-                Vector2 scaled = LocalPosition * Entity.Parent.Transform.Scale;
-
-                float cos = MathF.Cos(Entity.Parent.Transform.Rotation);
-                float sin = MathF.Sin(Entity.Parent.Transform.Rotation);
-                float x = scaled.X * cos - scaled.Y * sin;
-                float y = scaled.X * sin + scaled.Y * cos;
-
-                return new Vector2(x + Entity.Parent.Transform.Position.X, y + Entity.Parent.Transform.Position.Y);
-            }
-            set
-            {
-                if (Entity.IsRoot) LocalPosition = value;
-                else
-                {
-                    Vector2 delta = value - Entity.Parent.Transform.Position;
-
-                    float cos = MathF.Cos(-Entity.Parent.Transform.Rotation);
-                    float sin = MathF.Sin(-Entity.Parent.Transform.Rotation);
-                    float rx = delta.X * cos - delta.Y * sin;
-                    float ry = delta.X * sin + delta.Y * cos;
-
-                    LocalPosition = new Vector2(rx / Entity.Parent.Transform.Scale.X, ry / Entity.Parent.Transform.Scale.Y);
-                }
-            }
+            get => _position;
+            set => SetWorldPosition(value);
         }
 
         public float Rotation
         {
-            get
-            {
-                return Entity.IsRoot ? LocalRotation : Entity.Parent.Transform.Rotation + LocalRotation;
-            }
-            set
-            {
-                if (Entity.IsRoot) LocalRotation = value;
-                else LocalRotation = value - Entity.Parent.Transform.Rotation;
-            }
+            get => _rotation;
+            set => SetWorldRotation(value);
         }
 
         public Vector2 Scale
         {
-            get
-            {
-                if (Entity.IsRoot) return LocalScale;
+            get => _scale;
+            set => SetWorldScale(value);
+        }
 
-                return new Vector2(Entity.Parent.Transform.Scale.X * LocalScale.X, Entity.Parent.Transform.Scale.Y * LocalScale.Y);
-            }
-            set
+        public override void Update()
+        {
+            base.Update();
+
+            if (_localMatrixDirty)
             {
-                if (Entity.IsRoot) LocalScale = value;
-                else
-                {
-                    LocalScale = new Vector2(value.X / Entity.Parent.Transform.Scale.X, value.Y / Entity.Parent.Transform.Scale.Y);
-                }
+                _localMatrix = Matrix.CreateScale(new Vector3(_localScale, 1f)) * 
+                    Matrix.CreateRotationZ(_localRotation) * 
+                    Matrix.CreateTranslation(new Vector3(_localPosition, 0f));
+
+                _localMatrixDirty = false;
             }
+
+            if (Entity.IsRoot) _worldMatrix = _localMatrix;
+            else _worldMatrix = _localMatrix * Entity.Parent.Transform._worldMatrix;
+
+            Vector3 t = _worldMatrix.Translation;
+            _position = new Vector2(t.X, t.Y);
+
+            _rotation = MathF.Atan2(_worldMatrix.M12, _worldMatrix.M11);
+
+            _scale = new Vector2(
+                new Vector2(_worldMatrix.M11, _worldMatrix.M12).Length(),
+                new Vector2(_worldMatrix.M21, _worldMatrix.M22).Length()
+            );
+        }
+
+        private void SetWorldPosition(Vector2 worldPos)
+        {
+            if (Entity.IsRoot) LocalPosition = worldPos;
+            else
+            {
+                Matrix invParent = Matrix.Invert(Entity.Parent.Transform._worldMatrix);
+                Vector2 local3 = Vector2.Transform(worldPos, invParent);
+                LocalPosition = local3;
+            }
+        }
+
+        private void SetWorldRotation(float worldRot)
+        {
+            if (Entity.IsRoot) LocalRotation = worldRot;
+            else LocalRotation = worldRot - Entity.Parent.Transform._rotation;
+        }
+
+        private void SetWorldScale(Vector2 worldScale)
+        {
+            if (Entity.IsRoot) LocalScale = worldScale;
+            else LocalScale = new Vector2(worldScale.X / Entity.Parent.Transform._scale.X, worldScale.Y / Entity.Parent.Transform._scale.Y);
         }
     }
 }
